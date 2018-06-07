0852b30669d5918940f0fd88b3ecd8dea7d0c1f4
Eric Camalionte <eric80hz@yahoo.com.br>
2018-06-07 17:04:45 -0300

    Edit the home page with some development information

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Doctors Front Application</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className="App-intro">
+            The next step will be start the backend <code>../doctors-api/ mix phoenix.server</code> on localhost:4000 and fetch information
         </p>
       </div>
     );
