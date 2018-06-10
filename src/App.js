9d5b0160cba5b31d014f43d17e7985517046b926
Danielwsx64 <danielws.x64@gmail.com>
2018-06-10 18:40:44 -0300

    Clean project

--- src/App.js
+++ src/App.js
@@ -1,49 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Chat from './components/Chat.js';
 
 class App extends Component {
 
-    constructor(){
-        super();
-        this.state = { backend_connection: ''};
-    }
+  constructor(){
+    super();
+    this.state = { backend_connection: ''};
+  }
 
-    componentWillMount(){
-        let url = 'http://localhost:4000/api';
+  componentWillMount(){
+    let url = 'http://localhost:4000/api';
 
-        fetch(url)
-            .then(response => { return response.json(); } )
-            .then(json => { this.setState({backend_connection: json.data }); } )
-            .catch(error => { console.log(error); });
-    }
+    fetch(url)
+      .then(response => { return response.json(); } )
+      .then(json => { this.setState({backend_connection: json.data }) } )
+      .catch(error => { console.log(error); });
+  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Doctors Front Application</h1>
-        </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="App-intro">
-            Start the backend <code>doctors_api/ mix phoenix.server</code> on localhost:4000
-        </p>
-
-        <p className="App-intro">
-            Backend Connection Message:
-            <span className="Connection-ok">
+          Backend Connection Message:
+          <span className="Connection-ok">
             { this.state.backend_connection }
-            </span>
+          </span>
         </p>
 
         <Chat/>
       </div>
 
-    );
+      );
   }
 }
 
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -5,81 +5,82 @@ import ServerMessage from './ServerMessages.js';
 
 class Chat extends Component {
 
-    constructor(){
-        super();
-        this.state = {
-            inputMessage: '',
-            userMessages: [],
-            serverMessages: []
-        };
+  constructor(){
+    super();
+    this.state = {
+      inputMessage: '',
+      userMessages: [],
+      serverMessages: []
+    };
 
-        let url = "ws://localhost:4000/socket";
-        let socket = new Socket(url, {params: {token: window.userToken}});
-        socket.connect();
-        this.channel = socket.channel("room:lobby", {});
-        this.channel.join()
-            .receive("ok", response => { console.log("Joined successfully", response); });
-        this.channel.on("new_message", payload => {
-            this.setState({
-                serverMessages: this.state.serverMessages.concat(payload.body)
-            });
-            console.log("server messages: " + this.state.serverMessages);
-        });
-    }
+    let url = "ws://localhost:4000/socket";
+    let socket = new Socket(url, {params: {token: window.userToken}});
 
-    handleSubmit(event){
-        event.preventDefault();
-        this.channel.push("new_message", {body: this.state.inputMessage});
-        this.setState({
-            userMessages: this.state.userMessages.concat(this.state.inputMessage),
-            inputMessage: ""
-        });
-    }
+    socket.connect();
 
-    handleInputMessage(event){
-        this.setState({inputMessage: event.target.value});
-    }
+    this.channel = socket.channel("room:lobby", {});
+    this.channel.join()
+      .receive("ok", response => { console.log("Joined successfully", response); });
 
-    render() {
-        const userMessages = this.state.userMessages.map((message, index) =>
-            <UserMessage key={index}
-                         username= {"GenericUser"}
-                         message={message} />
-         );
+    this.channel.on("new_message", payload => {
+      this.setState({
+        serverMessages: this.state.serverMessages.concat(payload.body)
+      });
+      console.log("server messages: " + this.state.serverMessages);
+    });
+  }
 
-        const serverMessages = this.state.serverMessages.map((message, index) =>
-                                                             <ServerMessage key = { index }
-                                                             username = { "Server" }
-                                                             message = { message } />);
+  handleSubmit(event){
+    event.preventDefault();
+    this.channel.push("new_message", {body: this.state.inputMessage});
+    this.setState({
+      userMessages: this.state.userMessages.concat(this.state.inputMessage),
+      inputMessage: ""
+    });
+  }
 
-        return (
-         <div className="App">
-           <h1> Instant Message </h1>
+  handleInputMessage(event){
+    this.setState({inputMessage: event.target.value});
+  }
 
-           {userMessages}
-           {serverMessages}
+  render() {
+    const userMessages = this.state.userMessages.map((message, index) =>
+      <UserMessage key={index}
+        username= {"GenericUser"}
+        message={message} />
+      );
 
-           <div>
-                <form onSubmit={this.handleSubmit.bind(this)}>
-                   <div className="field">
-                     <label className="label"style={{ textAlign: "left" }}>
-                     GenericUsername:
-                     </label>
+    const serverMessages = this.state.serverMessages.map((message, index) =>
+      <ServerMessage key = { index }
+        username = { "Server" }
+        message = { message } />);
 
-                     <div className="control">
-                         <input className="input" type="text" style={{ marginTop: "10px"}} value={this.state.inputMessage } onChange={ this.handleInputMessage.bind(this) } />
-                     </div>
-                   </div>
+    return (
+      <div className="App">
+        <h1> Instant Message </h1>
 
-                   <button type="submit" value="Submit" className="button is-primary" style={{ marginTop: "10px" }} >
-                    Send Message
-                   </button>
-                </form>
-           </div>
+        {userMessages}
+        {serverMessages}
 
+        <div>
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <div className="field">
+              <label className="label"style={{ textAlign: "left" }}>
+                GenericUsername:
+              </label>
 
-         </div>
-        );
+              <div className="control">
+                <input className="input" type="text" style={{ marginTop: "10px"}} value={this.state.inputMessage } onChange={ this.handleInputMessage.bind(this) } />
+              </div>
+            </div>
+
+            <button type="submit" value="Submit" className="button is-primary" style={{ marginTop: "10px" }} >
+              Send Message
+            </button>
+          </form>
+        </div>
+      </div>
+      );
     }
 }
 
